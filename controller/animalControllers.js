6a2609a24069f538e1530453acddf79a314242d0
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-10-05 15:40:52 -0400

    controller/

---
+++ controller/animalControllers.js
@@ -0,0 +1,21 @@
+const Animal = require("../models/animalModel")
+
+exports.create = (req, res) => {
+
+}
+
+exports.findAll = (req, res) => {
+    
+}
+
+exports.findOne = (req, res) => {
+    
+}
+
+exports.update = (req, res) => {
+    
+}
+
+exports.delete = (req, res) => {
+    
+}
